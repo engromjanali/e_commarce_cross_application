74128beff2e1df1ad6004651b0d0ca6533ec22d4
engromjanalioffice <engromjanali@gmail.com>
2025-12-28 18:22:48 +0600

    added colors

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -8,18 +8,46 @@ import 'package:flutter/material.dart';
 /// - Uses `withValues` for dynamic alpha adjustments
 class PColors {
   // Core Colors
-  static const primaryColorLight = Color(0XFFFFFFFF);
-  static const primaryColorDark = Color(0xFF141416);
+  static const primaryColorLight = Color(0XFF1455AC);
+  static const primaryColorDark = Color(0XFF1455AC);
+
+  static const secondaryColorLight = Color(0XFFF58300);
+  static const secondaryColorDark = Color(0XFFF58300);
 
   // app bar
-  static const appBarColorLight = Color(0XFFFFFFFF);
+  static const appBarColorLight = Color(0XFFF58300);
   static const appBarColorDark = Color(0xFF000000);
   // nav bar
   static const nevColorLight = Color(0XFFFFFFFF);
   static const nevColorDark = Color(0xFF000000);
+
+  // Primary Light
+  static const lightColorLight = Color(0XFFE9F3FF);
+  static const lightColorDark = Color(0XFFE9F3FF);
+
   // card color
-  static const cardColorLight = Color(0x0D000000);
-  static const cardColorDark = Color(0xFF121214);
+  static const cardColorLight = Color(0XFFFFFFFF);
+  static const cardColorDark = Color(0XFFFFFFFF);
+
+  // Cancel/Negative/discount tag/ wishlist
+  static const taqColorLight = Color(0XFFFF5555);
+  static const taqColorDark = Color(0xFF121214);
+
+  // cancle/Negitive background
+  static const taqBackgroundColorLight = Color(0XFFFFF4F3);
+  static const taqBackgroundColorDark = Color(0XFFFFF4F3);
+
+  // warning color
+  static const warningColorLight = Color(0XFFFFAD31);
+  static const warningColorDark = Color(0XFFFFAD31);
+
+  // success color
+  static const successColorLight = Color(0XFF00AA6D);
+  static const successColorDark = Color(0XFF00AA6D);
+
+  // menu color
+  static const menuBackgroundColorLight = Color(0XFFF8F8F9);
+  static const menuBackgroundColorDark = Color(0xFF121214);
 
   // primary button color
   static const primaryButtonColorLight = Color(0xFF0088FF);
@@ -35,9 +63,9 @@ class PColors {
   static const secondaryFillColorLight = Color(0x1a0088FF);
   static const secondaryFillColorDark = Color(0xFF121214);
 
-  // Backgrounds
-  static const backGroundColorLight = Color(0xFFF0F0F0);
-  static const backGroundColorDark = Color(0xFF000000);
+  // scaffold Backgrounds
+  static const backGroundColorLight = Color(0XFFFCFCFC);
+  static const backGroundColorDark = Color(0XFF02070E);
 
   // Borders
   static final borderColorLight = Color(0xFFFFFFFF);
@@ -48,14 +76,14 @@ class PColors {
   static final dividerColorDark = Color(0xFF171717);
 
   // Text Colors
-  static const primayTextColorLight = Color(0XFF000000);
-  static const primayTextColorDark = Color(0XFFFFFFFF);
-  static const secondaryTextColorLight = Color(0XFF000000);
-  static const secondaryTextColorDark = Color(0XFFFFFFFF);
-  static const additionalTextColorLight = Colors.green;
-  static const additionalTextColorDark = Colors.green;
-  static const hintTextColorLight = Color(0xFFAAAAAA);
-  static const hintTextColorDark = Color(0xFFAAAAAA);
+  static const primayTextColorLight = Color(0XFF1C1E20);
+  static const primayTextColorDark = Color(0XFFFCFCFC);
+  static const secondaryTextColorLight = Color(0XFF595959);
+  static const secondaryTextColorDark = Color(0XFF595959);
+  static const deactiveTextColorLight = Color(0xFFAFB1B5);
+  static const deactiveTextColorDark = Color(0XFFAFB1B5);
+  static const categorySideBarColorLight = Color(0x2BAFB1B5);
+  static const categorySideBarColorDark = Color(0X2BAFB1B5);
 
   // image filter color
   static Gradient imageFG = LinearGradient(
@@ -70,6 +98,7 @@ class PColors {
       Colors.transparent, // top color
     ],
   );
+
   static var proLinearGradient = LinearGradient(
     begin: Alignment.topRight,
     end: Alignment.bottomLeft,
@@ -77,9 +106,7 @@ class PColors {
   );
 
   // indicator color,
-  static const pendingColor = Color(0xFFA8986B);
-  static const timeoutColor = Colors.red;
-  static const completedColor = Color(0xff004d4d);
-
-  //
+  // static const pendingColor = Color(0xFFA8986B);
+  // static const timeoutColor = Colors.red;
+  // static const completedColor = Color(0xff004d4d);
 }
